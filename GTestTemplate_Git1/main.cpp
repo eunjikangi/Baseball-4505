f99daf907e2d41e7a5cadb657c04e63696025a9d
eunjikangi <miki2323@naver.com>
2025-03-27 17:13:08 +0900

    BaseBall Game 실습 4/4

--- GTestTemplate_Git1/main.cpp
+++ GTestTemplate_Git1/main.cpp
@@ -1,4 +1,29 @@
 #include "gmock/gmock.h"
+#include "baseball.cpp"
+
+using namespace std;
+
+class BaseballFixture : public testing::Test {
+public:
+	Baseball game;
+	void assertIllegalArgument(string guessNumber) {
+		try
+		{
+			game.guess(string(guessNumber));
+			FAIL();
+		}
+		catch (exception e)
+		{
+			// PASS
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
+}
 
 int main() {
 	::testing::InitGoogleMock();
